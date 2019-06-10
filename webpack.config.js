8a2fd9a6be3c24d26187a4e30f9952939baebbfe
Victor Hallberg <victor@hallberg.cc>
2019-06-10 11:06:38 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/*eslint-env node*/
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = function(env) {
+  return {
+    mode: 'development',
+
+    entry: {
+      app: './index.js',
+    },
+
+    output: {
+      path: __dirname + '/dist',
+      filename: 'index.js',
+      publicPath: '/'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        inject: 'body',
+      }),
+    ],
+
+    bail: true,
+
+    devServer: {
+      contentBase: 'public',
+      inline: true,
+      port: 3000,
+    }
+  }
+}
